1a377d6bf6260b29ba8a34afa77ded99b74565c5
hahaharin <harin2950@naver.com>
2023-12-19 01:03:12 +0900

    [이코테] 곱하기 혹은 더하기

--- 이코테/greedy/곱하기 혹은 더하기.py
+++ 이코테/greedy/곱하기 혹은 더하기.py
@@ -5,8 +5,10 @@ result = int(data[0])
 
 for i in range(1,len(data)):
     num = int(data[i])
+    # 0 or 1 -> +
     if num <=1 or result <=1:
         result += num
+    # else -> *
     else:
         result *= num
 
