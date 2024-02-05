0b369ce481762a3b23b56d6cb19e2620be80b5fe
hahaharin <harin2950@naver.com>
2024-02-05 15:14:56 +0900

    [BOJ-7568] 덩치

---
+++ Baekjoon/구현/7568_덩치.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+array = [list(map(int, input().split())) for _ in range (n)]
+
+for i in array:
+    rank = 1
+    for j in array:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank +=1
+    print(rank, end=' ')
